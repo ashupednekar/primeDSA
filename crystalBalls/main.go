ecc4e569ec9cfad8b99a9061409dcbbd3ac3f7f1
Ashu Pednekar <ashupednekar49@gmail.com>
2025-02-14 00:41:34 +0530

    chore: crystal ball/ egg building done

--- crystalBalls/main.go
+++ crystalBalls/main.go
@@ -10,19 +10,18 @@ func findFloorWhereTheyBreak(floors []bool) (int, error){
   numFloors := len(floors)
   jumpOffset := int(math.Sqrt(float64(numFloors)))
 
-  floor := 0
-  jump := 0
 
-  for{
-    floor = floor + jump*jumpOffset
+  floor := jumpOffset
+
+  for floor < numFloors{
     if floors[floor]{
-      //broke, backtrack...
       break
     }
-    jump += 1
+    floor += jumpOffset
   }
 
-  for i:=jump*jumpOffset+1; i<floor; i++{
+  floor -= jumpOffset
+  for i:=floor; i<=floor+jumpOffset && i < numFloors; i++{
     if floors[i]{
       return i-1, nil
     }
@@ -38,6 +37,6 @@ func main(){
   if err != nil{
     fmt.Errorf("error: %s", err)
   }
-  fmt.Printf("floor where they break: %d", res)
+  fmt.Printf("last floor where they won't break: %d", res)
 
 }
